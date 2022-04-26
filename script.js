b1d32f0044e0e8f60571528ebb7f0d92e3f9647b
Jan Georg Henriksen <jangeorg85@gmail.com>
2022-04-26 03:01:39 +0200

    made animations

--- script.js
+++ script.js
@@ -9,3 +9,35 @@ const handleClick = (event)=>{
 links.forEach(item => {
   item.addEventListener('click', handleClick)
 })
+
+const page3 = document.querySelector('.h1-big2');
+const balls = document.querySelectorAll('.page3-div');
+const ballsArray = Array.from(balls);
+
+const finish = document.querySelector('.finish');
+
+const removeAnimationClasses =() => {
+  ballsArray.map(ball => ball.classList.remove('animate-balls'));
+  finish.classList.remove('animate-finish');
+}
+
+const animate = () => {
+  removeAnimationClasses();
+  ballsArray.map(ball => ball.classList.add('animate-balls'));
+  finish.classList.add('animate-finish');
+}
+
+const resetAnimate = () => {
+  removeAnimationClasses();
+  setTimeout(() => {
+    animate();
+  }, 500);
+}
+
+let observer = new IntersectionObserver((entries) => {
+  if (entries[0].isIntersecting) animate();
+});
+
+observer.observe(page3);
+
+document.querySelector('.restart-animation').addEventListener('click', resetAnimate);
